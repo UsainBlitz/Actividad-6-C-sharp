2886ad16e5f07883cb844e9b5c3099b445fddce4
Alonso López <78806606+UsainBlitz@users.noreply.github.com>
2023-04-25 12:06:00 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿{
+    "sdk": {
+        "version": "5.0.301"
+    },
+  "name": "Actividad_6_CSharp",
+  "version": "1.0.0",
+  "outputType": "Exe",
+  "targetFramework": "net5.0",
+  "nullable": "enable",
+  "nullableReferenceTypes": {
+        "enable": true
+  }
+}
